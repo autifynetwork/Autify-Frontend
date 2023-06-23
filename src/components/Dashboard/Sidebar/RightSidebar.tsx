b5967c8030673ef1fdfaee754a290df69efe5b36
pushpit07 <pushpit07@gmail.com>
2023-06-23 13:55:16 +0530

    Added siderbar chart
    Signed-off-by: pushpit07 <pushpit07@gmail.com>

--- src/components/Dashboard/Sidebar/RightSidebar.tsx
+++ src/components/Dashboard/Sidebar/RightSidebar.tsx
@@ -1,4 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion';
+import OrderStatusStats from './RightSidebar/OrderStatusStats';
 
 const RightSidebar = () => {
     return (
@@ -25,12 +26,33 @@ const RightSidebar = () => {
                         </span>
                     </div>
 
-                    <div className="flex flex-col justify-start items-center p-4 rounded-2xl font-semibold h-80 bg-primary-500 text-light-100">
+                    <div className="flex flex-col gap-y-5 justify-start items-center p-4 rounded-2xl font-semibold h-68 bg-primary-500 text-light-100">
                         Order Status Statistics
+                        <OrderStatusStats />
                     </div>
 
-                    <div className="flex flex-col justify-start items-center p-4 rounded-2xl font-semibold h-60 bg-primary-400 text-dark-800">
+                    <div className="flex flex-col justify-start items-center p-4 rounded-2xl font-semibold h-56 bg-primary-400 text-dark-800">
                         Ticket Status
+                        <div className="w-full mt-4 flex flex-col gap-y-6 text-sm font-normal">
+                            <div>
+                                <div className="text-[#7C828A]">XX of total XX</div>
+                                <div className="w-full rounded-full h-2.5 bg-[#7388A95A]">
+                                    <div className="bg-blue-600 h-2.5 w-1/3 rounded-full"></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="text-[#7C828A]">XX of total XX</div>
+                                <div className="w-full rounded-full h-2.5 bg-[#7388A95A]">
+                                    <div className="bg-[#EC6666] h-2.5 w-4/5 rounded-full"></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="text-[#7C828A]">XX of total XX</div>
+                                <div className="w-full rounded-full h-2.5 bg-[#7388A95A]">
+                                    <div className="bg-[#FFED1A] h-2.5 w-3/5 rounded-full"></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </motion.div>
---
+++ src/components/Dashboard/Sidebar/RightSidebar/OrderStatusStats.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from 'react';
+import Chart, { ChartConfiguration } from 'chart.js/auto';
+
+const OrderStatusStats: React.FC = () => {
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
+
+    useEffect(() => {
+        const data = {
+            labels: ['Lorem.', 'Lorem.', 'Lorem.', 'Lorem.'],
+            datasets: [
+                {
+                    label: 'Order Status',
+                    data: [280, 40, 50, 50],
+                    backgroundColor: [
+                        'rgb(246, 246, 246)',
+                        'rgb(255, 87, 165)',
+                        'rgb(121, 210, 222)',
+                        'rgb(245, 227, 6)',
+                    ],
+                    hoverOffset: 4,
+                },
+            ],
+        };
+
+        const chartConfig: ChartConfiguration = {
+            type: 'doughnut',
+            data: data,
+            options: {
+                // @ts-ignore
+                cutout: '80%',
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        labels: {
+                            usePointStyle: true,
+                            boxHeight: 8,
+                            boxWidth: 8,
+                            padding: 16,
+                            color: '#fff',
+                        },
+                    },
+                },
+                elements: {
+                    arc: {
+                        borderWidth: 4,
+                        borderColor: '#3B76EF',
+                        borderRadius: 16,
+                    },
+                },
+                layout: {
+                    padding: 1,
+                },
+            },
+        };
+
+        if (chartRef.current) {
+            const ctx = chartRef.current.getContext('2d');
+            if (ctx) {
+                const chartInstance = new Chart(ctx, chartConfig);
+                return () => {
+                    chartInstance.destroy();
+                };
+            }
+        }
+    }, []);
+
+    return <canvas ref={chartRef} style={{ width: '100px' }} />;
+};
+
+export default OrderStatusStats;
